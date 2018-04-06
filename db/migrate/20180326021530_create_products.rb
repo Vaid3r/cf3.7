7d18b14290d39a45b1d6e5c913df89b9cd718dca
vaid3r <mcluna1991@gmail.com>
2018-04-05 23:34:51 -0400

    added color and price field to products

--- db/migrate/20180326021530_create_products.rb
+++ db/migrate/20180326021530_create_products.rb
@@ -4,6 +4,8 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.string :name
       t.text :description
       t.string :image_url
+      t.string :color
+      t.decimal :price
 
       t.timestamps
     end
