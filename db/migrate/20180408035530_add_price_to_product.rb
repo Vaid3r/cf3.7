eb855b73f1b0e944d367f033618c836e10965c46
vaid3r <mcluna1991@gmail.com>
2018-04-08 00:12:17 -0400

    fixed price and color to display on site

--- db/migrate/20180408035530_add_price_to_product.rb
+++ db/migrate/20180408035530_add_price_to_product.rb
@@ -1,4 +1,4 @@
-class AddPriceToProducts < ActiveRecord::Migration[5.1]
+class AddPriceToProduct < ActiveRecord::Migration[5.1]
   def change
     add_column :products, :price, :decimal
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405183811) do
+ActiveRecord::Schema.define(version: 20180408035530) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
