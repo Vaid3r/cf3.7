ecb8b8bbd1ecaa0b09087d8f505d769baa18d9d2
vaid3r <mcluna1991@gmail.com>
2018-04-08 23:54:07 -0400

    fixed featured product page landing page to work

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
-  root 'simple_pages#index'
   root 'simple_pages#landing_page'
+
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
