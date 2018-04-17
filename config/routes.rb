69205e7af0d100f26e3edda1011e398da06dbf49
vaid3r <mcluna1991@gmail.com>
2018-04-16 22:46:54 -0400

    added orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'simple_pages/about'
 
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
 
 
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+  
 
 
 
