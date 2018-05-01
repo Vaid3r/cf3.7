c4f50fc01179b3092afe1a2c0b58e7f63a0496fb
vaid3r <mcluna1991@gmail.com>
2018-05-01 15:12:58 -0400

    error in partial

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,16 +4,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    if params[:q]
-      search_term = params[:q]
-      @products = Product.search(search_term)
-    else
-      @products = Product.all
+    @products = Product.all
   end
-
-
-
-
 
   # GET /products/1
   # GET /products/1.json
@@ -41,9 +33,9 @@ class ProductsController < ApplicationController
       else
         format.html { render :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
-
+      end
+    end
   end
-
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
@@ -55,9 +47,9 @@ class ProductsController < ApplicationController
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
-
+      end
+    end
   end
-
 
   # DELETE /products/1
   # DELETE /products/1.json
@@ -66,19 +58,17 @@ class ProductsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
-
+    end
   end
 
-
-
+  private
     # Use callbacks to share common setup or constraints between actions.
-  def set_product
+    def set_product
       @product = Product.find(params[:id])
-  end
-
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-  def product_params
-      params.require(:product).permit(:name, :description, :image_url, :color, :price)
-  end
+    def product_params
+      params.require(:product).permit(:name, :description, :image_url)
+    end
 end
