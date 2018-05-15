d76edd915fb2846c799f99660dcbdb54e016469b
vaid3r <mcluna1991@gmail.com>
2018-05-15 00:56:15 -0400

    5.9 pagination/validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,18 @@
 class Product < ApplicationRecord
+	#validates :name, presence: true
+	validates :name, :description, :image_url, :color, :price, presence: true
 
-  
-  has_many :orders
+	has_many :orders
 
-  has_many :comments
-
-
-  def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
-  end
+	has_many :comments
 
 
-  def average_rating
-  comments.average(:rating).to_f
-  end
+	def self.search(search_term)
+	Product.where("name LIKE ?", "%#{search_term}%")
+	end
 
+
+	def average_rating
+	comments.average(:rating).to_f
+	end
 end
