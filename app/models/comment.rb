f924d313652470ca08307bd6774a47272d0bb60f
vaid3r <mcluna1991@gmail.com>
2018-06-30 01:03:54 -0400

    6.15 fixes, test, products

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-    
-  scope :rating_asc, -> { order(rating: :asc) }
 
   validates :body, presence: true
   validates :user, presence: true
@@ -11,5 +9,8 @@ class Comment < ApplicationRecord
   validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
   #after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
   after_create_commit { CommentUpdateJob.perform_later(self, @user) }
+  
+  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_asc, -> { order(rating: :asc) }
 end
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
- 
-describe Comment do
-  
-  context "Testing validations" do
-    
-    it "is not valid without a rating" do
-      expect(Comment.new(rating:nil)).not_to be_valid
-    end
- 
 
-    it "is not valid without text" do 
-      expect(Comment.new(body:nil)).not_to be_valid
-    end
- 
-    it "is not valid without a user id" do
-      expect(Comment.new(user_id:nil)).not_to be_valid
-    end
- 
-    it "is not valid if a user doesn't use an integer" do
-      expect(Comment.new(user_id: 1, body:"abc", rating: "abc")).not_to be_valid
+describe Comment do
+  before(:each) do
+    @product = Product.create(name: "This is a test", description: "test", colour: "blue", price: 100)
+    @user = FactoryBot.create(:user)
+  end
+
+  context "If comment does not have all attributes" do
+    let(:comment) {Comment.new(user_id: 1, product_id: @product.id, body: "Nice board", rating: nil)}
+
+    it 'it is invalid' do
+      expect(Comment.new(user_id: 1, product_id: @product.id, body: "Nice board", rating: nil)).not_to be_valid
     end
   end
-end 
+
+  context "If comment has all attributes" do
+    let(:comment) {Comment.new(user_id: 1, product_id: @product.id, body: "Nice board", rating: 4)}
+
+    it 'it is valid' do
+      expect(Comment.new(user_id: 1, product_id: @product.id, body: "Nice board", rating: 4)).to be_valid    
+    end
+  end
+end
